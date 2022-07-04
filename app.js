2462897b24b8cb438dabf89b91e6693ca3317ee4
Brian Retamar <leo1632@hotmail.com>
2022-07-04 05:41:03 -0300

    feat: add custom middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const notFound = require('./middlewares/notFound.js')
 
 const app = express()
 
 // Router Middlewares
 app.use('/api/productos', require('./routes/products.routes'))
 app.use('/api/carrito', require('./routes/cart.routes'))
+
+// custom middlewares
+app.use(notFound)
 
 module.exports = app
