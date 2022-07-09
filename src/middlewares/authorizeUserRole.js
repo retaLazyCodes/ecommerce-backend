a5ba8257a859404316f72c1b86387dee19ffe097
Brian Retamar <leo1632@hotmail.com>
2022-07-09 18:28:58 -0300

    feat: add authorize user role middleware

---
+++ src/middlewares/authorizeUserRole.js
@@ -0,0 +1,18 @@
+module.exports = authorizeUserRole
+
+function authorizeUserRole (role = false) {
+  return [
+    (req, res, next) => {
+      if (role !== true) {
+        // user's role is not authorized
+        return res.status(401).json(
+          {
+            error: -1,
+            descripcion: 'ruta: ' + req.originalUrl + ' metodo: ' + req.method + ' no autorizada'
+          })
+      }
+      // authentication and authorization successful!!
+      next()
+    }
+  ]
+}
