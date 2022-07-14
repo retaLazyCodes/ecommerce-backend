e80061fbf1f1b95e50f4afc34c36f61cec63d25e
Brian Retamar <leo1632@hotmail.com>
2022-07-14 09:51:11 -0300

    refactor: fixed file format

--- src/config/index.js
+++ src/config/index.js
@@ -2,19 +2,19 @@ const DEV_PORT = 8080
 const USER_ADMIN = true
 
 const config = {
-    FILESYSTEM_DB: {
-        products: 'products.json',
-        carts: 'carts.json'
-    },
-    USER_ADMIN,
-    server: {
-        PORT: process.env.PORT ? process.env.PORT : DEV_PORT,
-        routes: {
-            base: '/api',
-            products: '/api/productos',
-            carts: '/api/carrito',
-        },
-    },
-};
+  FILESYSTEM_DB: {
+    products: 'products.json',
+    carts: 'carts.json'
+  },
+  USER_ADMIN,
+  server: {
+    PORT: process.env.PORT ? process.env.PORT : DEV_PORT,
+    routes: {
+      base: '/api',
+      products: '/api/productos',
+      carts: '/api/carrito'
+    }
+  }
+}
 
 module.exports = { config }
